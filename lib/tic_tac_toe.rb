4923a409558569c9b8aada93da61e4c38442093d
Monatric <nawpnawp8@gmail.com>
2024-06-26 22:14:29 +0800

    Add few initialized variables

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,9 @@
+# insert class description
+class TicTacToe
+  def initialize
+    @is_game_finished = false
+    @player1 = "Player 1"
+    @player2 = "Player 2"
+    @next_turn = @player_1
+  end
+end
