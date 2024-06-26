01a725a48500596347344b3f2d34c5372bb54b9f
Monatric <nawpnawp8@gmail.com>
2024-06-26 22:27:45 +0800

    Output some texts for the game

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,4 +6,15 @@ class TicTacToe
     @player2 = "Player 2"
     @next_turn = @player_1
   end
+
+  def start_game
+    puts "The game has begun. It is Player 1's turn. Your letter is X."
+    puts "Enter a number where you want to place the letter."
+    puts "The current board             Guide"
+    puts "   |   |                    1 | 2 | 3 "
+    puts "---|---|---                ---|---|---"
+    puts "   |   |                    4 | 5 | 6 "
+    puts "---|---|---                ---|---|---"
+    puts "   |   |                    7 | 8 | 9 "
+  end
 end
