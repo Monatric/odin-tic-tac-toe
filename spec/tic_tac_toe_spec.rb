a2c0cb80135e996b3d4c6c37535350d19b6f368d
Monatric <nawpnawp8@gmail.com>
2024-10-14 01:20:49 +0800

    Configure rubocop so that it ignores rspec block lengths

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,34 +1,34 @@
-require_relative "../lib/tic_tac_toe"
+require_relative '../lib/tic_tac_toe'
 # when the #check_combination's array contains a value \"X\" (save for later)
 
 describe TicTacToe do
   subject(:game) { described_class.new }
-  describe "#player_has_won?" do
-    context "when the board shows a straight three X" do
+  describe '#player_has_won?' do
+    context 'when the board shows a straight three X' do
       before do
-        allow(game).to receive(:check_combination).and_return(["X"])
+        allow(game).to receive(:check_combination).and_return(['X'])
       end
-      it "returns true" do
+      it 'returns true' do
         result = game.player_has_won?
         expect(result).to be true
       end
     end
 
-    context "when the board shows a straight three O" do
+    context 'when the board shows a straight three O' do
       before do
-        allow(game).to receive(:check_combination).and_return(["O"])
+        allow(game).to receive(:check_combination).and_return(['O'])
       end
-      it "returns true" do
+      it 'returns true' do
         result = game.player_has_won?
         expect(result).to be true
       end
     end
 
-    context "when the board does not have straight three letters" do
+    context 'when the board does not have straight three letters' do
       before do
-        allow(game).to receive(:check_combination).and_return([" "])
+        allow(game).to receive(:check_combination).and_return([' '])
       end
-      it "returns false" do
+      it 'returns false' do
         result = game.player_has_won?
         expect(result).to be false
       end
